077152a9e6fcddd56f673795e03dca96fc28a53f
Camilobarre <barrecami@gmail.com>
2024-08-05 07:51:26 -0500

    feat: Se crean las propiedades de la clase VeterinaryClinic

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace FiltroCSharp_CamiloBarreneche.Models
 {
+    // Se crean las propiedades de la clase VeterinaryClinic 
     public class VeterinaryClinic
     {
-        
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public List<Dog> Dogs { get; set; }
+        public List<Cat> Cats { get; set; }
     }
 }
