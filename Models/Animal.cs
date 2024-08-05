73d5c278f61a8930237248cf5fe7dac5405ad2d0
Camilobarre <barrecami@gmail.com>
2024-08-05 06:35:15 -0500

    feat: Se crea un constructor para la clase Animal

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -15,5 +15,16 @@ namespace FiltroCSharp_CamiloBarreneche.Models
         protected string Breed { get; set; }
         protected string Color { get; set; }
         protected double WeightInKg { get; set; }
+
+        // Constructor de la clase Animal
+        public Animal(int id, string name, DateOnly birthDate, string breed, string color, double weightInKg)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.Breed = breed;
+            this.Color = color;
+            this.WeightInKg = weightInKg;
+        }
     }
 }
