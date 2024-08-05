0b76e7029ac5dc236eefbbead7b1ff12245abe9a
Camilobarre <barrecami@gmail.com>
2024-08-05 07:12:00 -0500

    feat: Se crea un constructor para la clase Dog

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -9,10 +9,20 @@ namespace FiltroCSharp_CamiloBarreneche.Models
     {
         // Se crean las propiedades de la clase Dog
         public bool BreedingStatus { get; set; }
-        public string Temperament { get; set; }
-        public string MicrochipNumber { get; set; }
-        public string BarkVolume { get; set; }
-        public string CoatType { get; set; }
+        public string? Temperament { get; set; }
+        public string? MicrochipNumber { get; set; }
+        public string? BarkVolume { get; set; }
+        public string? CoatType { get; set; }
 
+        // Constructor de Dog como herencia de Animal
+        public Dog(int id, string name, DateOnly birthDate, string breed, string color, double weightInKg, bool breedingStatus, string temperament,
+        string microchipNumber, string barkVolume, string coatType) : base(id, name, birthDate, breed, color, weightInKg)
+        {
+            this.BreedingStatus = breedingStatus;
+            this.Temperament = temperament;
+            this.MicrochipNumber = microchipNumber;
+            this.BarkVolume = barkVolume;
+            this.CoatType = coatType;
+        }
     }
 }
