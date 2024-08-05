20f5db149dfc92b9e4d568557b36966e764f812e
Camilobarre <barrecami@gmail.com>
2024-08-05 08:39:38 -0500

    feat: Se crea el método SaveDog

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -26,5 +26,44 @@ namespace FiltroCSharp_CamiloBarreneche.Models
             this.Dogs = new List<Dog>();
             this.Cats = new List<Cat>();
         }
+
+        // Método para guardar un perro
+        public void SaveDog(Dog newDog)
+        {
+            Console.WriteLine("Ingresa los datos del perrito: ");
+
+            Console.Write("Id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nombre: ");
+            string? name = Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Fecha de Nacimiento: ");
+            DateOnly birthDate = DateOnly.FromDateTime(Convert.ToDateTime(Console.ReadLine()));
+
+            Console.Write("Raza: ");
+            string? breed=Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Color: ");
+            string? color=Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Peso en KG: ");
+            double? weightInKg =Convert.ToByte(Console.ReadLine());
+
+            Console.Write("Estado de la cría: ");
+            bool? breedingStatus = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("Temperamento: ");
+            string? temperament= Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Número de Microchip: ");
+            string? microchipNumber = Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Volumen de Ladrido: ");
+            string? barkVolume= Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Tipo de Pelaje: ");
+            string? coatType= Console.ReadLine().Trim().ToLower();
+        }
     }
 }
